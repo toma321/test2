94677cd80616cfa2bc2530977931d73ce5f95001
toma.radu <toma.radu>
2018-05-23 22:52:12 +0300

    hello

---
+++ test2/app/src/main/java/practicaltest02/eim/systems/cs/pub/ro/test/network/ClientThread.java
@@ -0,0 +1,76 @@
+package practicaltest02.eim.systems.cs.pub.ro.test.network;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import practicaltest02.eim.systems.cs.pub.ro.test.Constants;
+import practicaltest02.eim.systems.cs.pub.ro.test.Utilities;
+
+public class ClientThread extends Thread {
+
+    private String address;
+    private int port;
+    private String locationUrl;
+    private TextView weatherForecastTextView;
+
+    private Socket socket;
+
+    public ClientThread(String address, int port, String locationUrl, TextView weatherForecastTextView) {
+        this.address = address;
+        this.port = port;
+        this.locationUrl = locationUrl;
+        this.weatherForecastTextView = weatherForecastTextView;
+    }
+
+    @Override
+    public void run() {
+        try {
+            socket = new Socket(address, port);
+            if (socket == null) {
+                Log.e(Constants.TAG, "[CLIENT THREAD] Could not create socket!");
+                return;
+            }
+            BufferedReader bufferedReader = Utilities.getReader(socket);
+            PrintWriter printWriter = Utilities.getWriter(socket);
+            if (bufferedReader == null || printWriter == null) {
+                Log.e(Constants.TAG, "[CLIENT THREAD] Buffered Reader / Print Writer are null!");
+                return;
+            }
+
+            printWriter.println(locationUrl);
+            printWriter.flush();
+            String weatherInformation;
+            while ((weatherInformation = bufferedReader.readLine()) != null) {
+                final String finalizedWeateherInformation = weatherInformation;
+                weatherForecastTextView.post(new Runnable() {
+                   @Override
+                    public void run() {
+                       weatherForecastTextView.setText(weatherForecastTextView.getText() + finalizedWeateherInformation);
+                   }
+                });
+            }
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "[CLIENT THREAD] An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    Log.e(Constants.TAG, "[CLIENT THREAD] An exception has occurred: " + ioException.getMessage());
+                    if (Constants.DEBUG) {
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ test2/app/src/main/java/practicaltest02/eim/systems/cs/pub/ro/test/network/CommunicationThread.java
@@ -0,0 +1,106 @@
+package practicaltest02.eim.systems.cs.pub.ro.test.network;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.ResponseHandler;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.impl.client.BasicResponseHandler;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import practicaltest02.eim.systems.cs.pub.ro.test.Constants;
+import practicaltest02.eim.systems.cs.pub.ro.test.Utilities;
+
+public class CommunicationThread extends Thread {
+
+    private ServerThread serverThread;
+    private Socket socket;
+
+    public CommunicationThread(ServerThread serverThread, Socket socket) {
+        this.serverThread = serverThread;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        if (socket == null) {
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] Socket is null!");
+            return;
+        }
+        try {
+            BufferedReader bufferedReader = Utilities.getReader(socket);
+            PrintWriter printWriter = Utilities.getWriter(socket);
+            if (bufferedReader == null || printWriter == null) {
+                Log.e(Constants.TAG, "[COMMUNICATION THREAD] Buffered Reader / Print Writer are null!");
+                return;
+            }
+            Log.i(Constants.TAG, "[COMMUNICATION THREAD] Waiting for parameters from client (city / information type!");
+            String urlLink = bufferedReader.readLine();
+
+            if (urlLink == null || urlLink.isEmpty()) {
+                Log.e(Constants.TAG, "[COMMUNICATION THREAD] Error receiving parameters from client (city / information type!");
+                return;
+            }
+            HashMap<String, Document> data = serverThread.getData();
+            Document document = null;
+            if (data.containsKey(urlLink)) {
+                Log.i(Constants.TAG, "[COMMUNICATION THREAD] Getting the information from the cache...");
+                document = data.get(urlLink);
+            } else {
+                Log.i(Constants.TAG, "[COMMUNICATION THREAD] Getting the information from the webservice...");
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpGet httpPost = new HttpGet(urlLink);
+
+                ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                String pageSourceCode = httpClient.execute(httpPost, responseHandler);
+                if (pageSourceCode == null) {
+                    Log.e(Constants.TAG, "[COMMUNICATION THREAD] Error getting the information from the webservice!");
+                    return;
+                }
+                document = Jsoup.parse(pageSourceCode);
+
+
+            }
+            if (document == null) {
+                Log.e(Constants.TAG, "[COMMUNICATION THREAD] Weather Forecast Information is null!");
+                return;
+            }
+
+            printWriter.println(document.toString());
+            printWriter.flush();
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + ioException.getMessage());
+                    if (Constants.DEBUG) {
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+}
